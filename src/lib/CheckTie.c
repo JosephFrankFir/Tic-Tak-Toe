b27173a28b65af9be218118bdad84976a5f3c0f9
Joseph <joseph@Ubuntu.localdomain>
2022-07-24 21:48:59 +0200

    --save argument

--- src/lib/CheckTie.c
+++ src/lib/CheckTie.c
@@ -1,7 +1,7 @@
 #include "../include/game.h"
 
 bool CheckTie(void)
-{
+{ 
 
     if (
         IsMarkAble[0] == false && IsMarkAble[1] == false && IsMarkAble[2] == false && IsMarkAble[3] == false && IsMarkAble[4] == false && IsMarkAble[5] == false && IsMarkAble[6] == false && IsMarkAble[7] == false && IsMarkAble[8] == false)
